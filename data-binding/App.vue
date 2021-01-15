156db28a1522f38f5599a4dff3031eda59e86682
Amanda Gonsalves <amandagonsalves75@gmail.com>
2021-01-15 15:43:52 -0300

    Add files via upload

---
+++ data-binding/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1 :style="{color}">Hello {{ name }}</h1>
+    <input v-model="name">
+    <a :href="link">Telegram</a>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    name: 'Amanda',
+    color: 'tomato',
+    link: 'https://t.me/vuejsbrasil'
+  })
+}
+</script>
+
+<style>
+
+</style>
---
+++ directives/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <h1>Lista de tarefas</h1>
+    <button @click="() => showList = !showList">Ver lista</button>
+    <input v-focus>
+    <table v-if="showList">
+      <thead>
+        <tr>
+          <th>Título</th>
+          <th>Feita</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr v-for="(task, index) in tasks" :key="`${task}-${index}`">
+          <td>{{ task.title }}</td>
+          <td>{{ task.isDone ? 'Sim' : 'Não' }}</td>
+        </tr>
+      </tbody>
+
+    </table>
+
+    <p v-else>Lista de tarefas escondida</p>
+  </div>
+</template>
+
+<script>
+const focus = {
+  inserted: (el) => {
+    el.focus();
+  }
+}
+
+export default {
+  directives: {
+    focus
+  },
+  data: () => ({
+    showList: false,
+    tasks:[
+      {
+        title: 'Fazer um curso',
+        isDone: true
+      },
+      {
+        title: 'Fazer comida',
+        isDone: false
+      }
+    ]
+  })
+}
+</script>
+
+<style scoped>
+  table {
+    text-align: center;
+    width:100%
+  }
+
+  td, th {
+    padding: .3rem;
+  }
+</style>

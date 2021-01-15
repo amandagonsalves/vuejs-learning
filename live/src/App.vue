d3dc765bc9fe6553b4dda3a52c2442a1b21ae171
Amanda Gonsalves <amandagonsalves75@gmail.com>
2021-01-15 15:54:14 -0300

    update

---
+++ live/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>My list</h1>
+    <input 
+      @keyup.enter="addFood" 
+      type="text" 
+      v-model="currentFood" />
+    <button @click="addFood">Add</button>
+
+    <FoodsList 
+      @stop="handleStop"
+      :foods="foods" />
+  </div>
+</template>
+
+<script>
+import FoodsList from './components/FoodsList.vue';
+
+export default {
+  components: {
+    FoodsList,
+  },
+
+  data: () => ({
+    foods: ["Maçã", "Banana", "Abacaxi", "Pera", "Morango"],
+    currentFood: "",
+  }),
+
+  methods: {
+    addFood() {
+      this.foods.push(this.currentFood);
+      this.currentFood = "";
+    },
+
+    handleStop(payload) {
+      console.log('payload =>', payload);
+      window.alert(payload)
+    }
+  },
+};
+</script>
---
+++ live/src/components/FoodsList.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul>
+    <li v-for="(food, index) in foodsWithDots" :key="`${food}-${index}`">
+      {{ food }}
+    </li>
+  </ul>
+</template>
+
+<script>
+const maxFoods = 10;
+
+export default {
+  props: {
+    foods: { type: Array, default: () => [] },
+  },
+  data () {
+    return {
+    foodsWithDots: this.foods.map(food => `${food}.`) 
+    }
+  },
+  watch: {
+    foods() {
+      if (this.foods.length === maxFoods) {
+        this.$emit('stop', 'ta cheio')
+        console.log("changed", this.foods.length);
+      }
+    },
+  },
+};
+</script>
